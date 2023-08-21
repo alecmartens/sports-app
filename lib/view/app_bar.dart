4110d729a7fd84e2be1c7ba4ec6f8136d66f4c3e
Alec Martens <am400316@ohio.edu>
2023-08-20 20:43:46 -0400

    Added logo

--- lib/view/app_bar.dart
+++ lib/view/app_bar.dart
@@ -2,24 +2,30 @@ import 'package:flutter/material.dart';
 
 PreferredSizeWidget topAppBar() {
   return AppBar(
-    title: const Text(
-      "Alec's Sports",
-      style: TextStyle(
-        color: Colors.lightBlue,
-        fontWeight: FontWeight.bold,
-      ),
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/images/logos/small_monke.png',
+          fit: BoxFit.contain,
+          height: 32,  // You can adjust the size as needed.
+        ),
+        const SizedBox(width: 8),  // Add a little spacing between logo and title
+        const Text(
+          "Alec's Sports",
+          style: TextStyle(
+            color: Colors.lightBlue,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     ),
     centerTitle: true,
     backgroundColor: Colors.transparent,
     elevation: 0,
     flexibleSpace: Container(
       decoration: const BoxDecoration(
-        color: Colors.black
-        // gradient: LinearGradient(
-        //   colors: [Colors.blue, Colors.black],
-        //   begin: Alignment.topLeft,
-        //   end: Alignment.bottomRight,
-        // ),
+        color: Colors.black,
       ),
     ),
     leading: IconButton(
@@ -32,10 +38,10 @@ PreferredSizeWidget topAppBar() {
         child: Icon(Icons.person, color: Colors.black),
       ),
       const SizedBox(width: 10),
-      IconButton(
-        icon: const Icon(Icons.notifications, color: Colors.lightBlue),
-        onPressed: () {},
-      ),
+      // IconButton(
+      //   icon: const Icon(Icons.notifications, color: Colors.lightBlue),
+      //   onPressed: () {},
+      // ),
     ],
   );
 }
